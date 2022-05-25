d14fe1520d83471c6a40fc7786f8b45ad286a05a
sverwer <sicco@Siccos-MacBook-Pro.local>
2022-05-25 14:42:12 +0200

    =files modified by build process

--- source/evaluators.h
+++ source/evaluators.h
@@ -1,17 +1,19 @@
-#ifndef __ALL_HEADERS__
-#define __ALL_HEADERS__
-#include "evaluation/aic.h"
-#include "evaluation/alergia.h"
-#include "evaluation/alergia94.h"
-#include "evaluation/count_types.h"
-#include "evaluation/edsm.h"
-#include "evaluation/evaluate.h"
-#include "evaluation/gini.h"
-#include "evaluation/kldistance.h"
-#include "evaluation/likelihood.h"
-#include "evaluation/mealy.h"
-#include "evaluation/mse_error.h"
-#include "evaluation/overlap.h"
-#include "evaluation/overlap4logs.h"
-#include "evaluation/rti+.h"
+#ifndef __ALL_HEADERS__ 
+#define __ALL_HEADERS__ 
+
+#include "aic.h"
+#include "alergia.h"
+#include "alergia94.h"
+#include "count_types.h"
+#include "edsm.h"
+#include "evaluate.h"
+#include "gini.h"
+#include "kldistance.h"
+#include "likelihood.h"
+#include "mealy.h"
+#include "mse_error.h"
+#include "overlap.h"
+#include "overlap4logs.h"
+#include "rti+.h"
+
 #endif
