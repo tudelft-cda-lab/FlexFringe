10e95c1a3e01d2de1eac39e8b503aad9646116f6
sverwer <sicco@Siccos-MacBook-Pro.local>
2022-08-24 11:50:10 +0200

    =fixed access trace nullpointer due to not addint tails

--- source/apta.cpp
+++ source/apta.cpp
@@ -384,8 +384,10 @@ void apta_guard::initialize(apta_guard* g){
 }
 
 void apta_node::add_tail(tail* t){
-    t->next_in_list = tails_head;
-    tails_head = t;
+    if(ADD_TAILS){
+        t->next_in_list = tails_head;
+        tails_head = t;
+    }
 
     if(access_trace == nullptr){
         if(t->past() != nullptr)
