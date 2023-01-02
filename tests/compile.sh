d5d61016401c847f703073e4196a7d9281fb7e2f
Tom Catshoek <tomcatshoek@zeelandnet.nl>
2023-01-02 17:58:32 +0100

    clean up tests dir

--- tests/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++11 -DUNIT_TESTING=1 -Wall -I../source/utility -I/home/chris/Dropbox/dfasat/source/evaluation -I/home/chris/Dropbox/dfasat/source/ -I/home/chris/Dropbox/dfasat -I/home/chris/Dropbox/dfasat/source/utility -o tests tests.cpp tail.cpp ../source/*.cpp ../source/evaluation/*.o -lm -lpopt -lgsl -lgslcblas -lpthread -ldl
--- tests/runtests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++11 -Wall -Iutility -o tests tests.o 020-Test-Case-1.cpp
-
