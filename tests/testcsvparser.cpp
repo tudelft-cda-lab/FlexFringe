ac3dd763cdd82cfaa38c9575e895d6adf9a301ab
Tom Catshoek <tomcatshoek@zeelandnet.nl>
2023-01-06 19:17:08 +0100

    CSV parsing sort of works

--- tests/testcsvparser.cpp
+++ tests/testcsvparser.cpp
@@ -13,15 +13,21 @@ TEST_CASE("csv_parser: smoke test", "[parsing]") {
     std::istringstream inputstream(input);
 
     auto parser = csv_parser(
-            std::make_unique<csv::CSVReader>(inputstream)
+            std::make_unique<csv::CSVReader>(
+                    inputstream,
+                    csv::CSVFormat().trim({' '})
+                    )
     );
 
     auto first = parser.next().value();
     REQUIRE(first.get("id") == std::vector<std::string> {"1"});
+    REQUIRE(first.get("symb") == std::vector<std::string> {"a"});
 
     auto second = parser.next().value();
     REQUIRE(second.get("id") == std::vector<std::string> {"2"});
+    REQUIRE(second.get("symb") == std::vector<std::string> {"b"});
 
     auto third = parser.next().value();
     REQUIRE(third.get("id") == std::vector<std::string> {"3"});
+    REQUIRE(third.get("symb") == std::vector<std::string> {"c"});
 }
