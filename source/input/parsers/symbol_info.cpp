dcaaa60e2844c2c0ac68a8c720632dba913baddf
Tom Catshoek <tomcatshoek@zeelandnet.nl>
2023-01-09 16:52:09 +0100

    Add convenience method to add single element to symbolinfo instead of vector

--- source/input/parsers/symbol_info.cpp
+++ source/input/parsers/symbol_info.cpp
@@ -11,3 +11,7 @@ const std::vector<std::string> &symbol_info::get(const std::string &name) const 
 void symbol_info::set(const std::string &name, const std::vector<std::string> &property_list) {
     properties.emplace(name, property_list);
 }
+
+void symbol_info::set(const std::string &name, const std::string &property) {
+    properties.emplace(name, std::vector<std::string> {property});
+}
