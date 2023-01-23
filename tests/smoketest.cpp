cbe24fc6a3f83848c788394dfa108d998287dc7a
Tom Catshoek <tomcatshoek@zeelandnet.nl>
2023-01-23 14:00:31 +0100

    Add additional checks to smoke test 1

--- tests/smoketest.cpp
+++ tests/smoketest.cpp
@@ -31,18 +31,24 @@ TEST_CASE( "Smoke test: greedy alergia on stamina 1_training", "[smoke]" ) {
     auto parser = abbadingoparser(input_stream);
     id.read(&parser);
 
-    apta* the_apta = new apta();
-    auto* merger = new state_merger(&id, eval, the_apta);
-    the_apta->set_context(merger);
-    eval->set_context(merger);
+    apta the_apta;
+    state_merger merger(&id, eval, &the_apta);
+    the_apta.set_context(&merger);
+    eval->set_context(&merger);
 
     eval->initialize_before_adding_traces();
-    id.add_traces_to_apta(the_apta);
-    eval->initialize_after_adding_traces(merger);
+    id.add_traces_to_apta(&the_apta);
+    eval->initialize_after_adding_traces(&merger);
 
-    greedy_run(merger);
+    // Double check all apta nodes have an access trace
+    for(APTA_iterator Ait = APTA_iterator(the_apta.get_root()); *Ait != 0; ++Ait) {
+        apta_node *n = *Ait;
+        assert(n->get_access_trace() != nullptr);
+    }
 
-//    print_current_automaton(merger, "/tmp/flexfringe_test_out", ".final");
+    greedy_run(&merger);
+
+    // print_current_automaton(&merger, "/tmp/flexfringe_test_out", ".final");
 
     //TODO: verify learned state machine is reasonable
 
