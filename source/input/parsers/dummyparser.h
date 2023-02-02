db80f314002ce9a7613c9d020e6444cb49a7d506
Tom Catshoek <tomcatshoek@zeelandnet.nl>
2023-02-02 14:36:56 +0100

    WIP

---
+++ source/input/parsers/dummyparser.h
@@ -0,0 +1,23 @@
+//
+// Created by tom on 1/30/23.
+//
+
+#ifndef FLEXFRINGE_DUMMYPARSER_H
+#define FLEXFRINGE_DUMMYPARSER_H
+
+#include <deque>
+
+#include "input/parsers/symbol_info.h"
+#include "input/parsers/i_parser.h"
+
+class dummyparser: parser {
+private:
+    std::deque<symbol_info> symbols;
+
+public:
+    dummyparser& add(const symbol_info& s);
+    std::optional<symbol_info> next() override;
+};
+
+
+#endif //FLEXFRINGE_DUMMYPARSER_H
