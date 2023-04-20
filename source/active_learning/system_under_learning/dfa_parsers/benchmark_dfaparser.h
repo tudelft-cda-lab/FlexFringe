70c8c76bffa929bec940c85362a6e335ea9e6e63
Robert Baumgartner <r.baumgartner-1@tudelft.nl>
2023-04-20 11:46:02 +0800

    Started an initial parser for the benchmarks. A little bit of restructuring to fit those in

---
+++ source/active_learning/system_under_learning/dfa_parsers/benchmark_dfaparser.h
@@ -0,0 +1,33 @@
+/**
+ * @file benchmark_dfaparser.h
+ * @author Robert Baumgartner (r.baumgartner-1@tudelft.nl)
+ * @brief A class to connect to the .dot files given by https://automata.cs.ru.nl/Syntax/Overview
+ * IMPORTANT: Only DFAs supported in this moment.
+ * @version 0.1
+ * @date 2023-04-20
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef _BENCHMARK_DFAPARSER_H_
+#define _BENCHMARK_DFAPARSER_H_
+
+#include "benchmarkparser_base.h"
+
+/**
+ * @brief This base class parses the files from the benchmark given by https://automata.cs.ru.nl/Syntax/Overview
+ * For now we only implement the DFAs.
+ * 
+ */
+class benchmark_dfaparser : public benchmarkparser_base {
+protected:
+    virtual std::unique_ptr<apta> read_input(ifstream& input_stream) const override;
+    
+    virtual std::optional<line_info> readline(ifstream& input_stream) const override;
+public:
+    benchmark_dfaparser() = default;
+};
+
+
+#endif
---
+++ source/active_learning/system_under_learning/dfa_parsers/benchmarkparser_base.h
@@ -0,0 +1,45 @@
+/**
+ * @file benchmarkparser_base.h
+ * @author Robert Baumgartner (r.baumgartner-1@tudelft.nl)
+ * @brief Base class to parser from the benchmark given by https://automata.cs.ru.nl/Syntax/Overview
+ * 
+ * @version 0.1
+ * @date 2023-04-20
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef _BENCHMARKPARSER_BASE_H_
+#define _BENCHMARKPARSER_BASE_H_
+
+#include "apta.h"
+#include "line_info.h"
+
+#include <fstream>
+#include <memory>
+#include <optional>
+#include <list>
+
+/**
+ * @brief This base class parses the files from the benchmark given by https://automata.cs.ru.nl/Syntax/Overview
+ * For now we only implement the DFAs.
+ * 
+ */
+class benchmarkparser_base {
+protected:
+    /**
+    * @brief Parses the input file, returns a ready apta to be used by the dfa_sul.
+    * 
+    * @return std::unique_ptr<apta> The ready apta.
+    */
+    virtual std::unique_ptr<apta> read_input(ifstream& input_stream) const = 0;
+
+    virtual std::optional<line_info> readline(ifstream& input_stream) const = 0;
+public:
+    benchmarkparser_base() = default;
+    std::unique_ptr<apta> get_apta() const;
+};
+
+
+#endif
