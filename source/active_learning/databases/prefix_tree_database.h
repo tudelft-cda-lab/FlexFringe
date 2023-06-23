6fa584c6d5eba9caad97562d3e9e16c516fd0b8b
Robert Baumgartner <r.baumgartner-1@tudelft.nl>
2023-06-23 12:51:09 +0200

    Went further with the algorithm. Next step build and APTA using the inputdata, so that I can use that one for statistics and the database SUT I am building

--- source/active_learning/databases/prefix_tree_database.h
+++ source/active_learning/databases/prefix_tree_database.h
@@ -1,7 +1,9 @@
 /**
  * @file prefix_tree_database.h
  * @author Robert Baumgartner (r.baumgartner-1@tudelft.nl)
- * @brief 
+ * @brief This database simply uses an AUT to store input traces. It can then be further used by the 
+ * database-SUT to obtain statistics and memberships.
+ *  
  * @version 0.1
  * @date 2023-06-22
  * 
@@ -13,23 +15,21 @@
 #define PREFIX_DATABASE
 
 #include "database_base.h"
+#include "apta.h"
 
 #include <unordered_map>
 #include <fstream>
+#include <memory>
 
-class prefix_database : public database_base {
+class prefix_tree_database : public database_base {
   protected:
 
-/*     struct prefix_tree_node{
-      typedef 
-      public:
-        unordered_map<>
-    } */
+    std::unique_ptr<apta> the_tree;
 
-    virtual void initialize(std::ifstream& input);
+    virtual void initialize();
   public:
-    prefix_database(std::ifstream& input){
-      initialize(input);
+    prefix_tree_database(){
+      database_base::database_base();
     }
 };
 
