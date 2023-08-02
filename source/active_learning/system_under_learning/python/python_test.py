14abbb90d449a2c83a8afb506749686a6ebe5378
Robert Baumgartner <r.baumgartner-1@tudelft.nl>
2023-08-02 10:53:50 +0200

    Test for function calls work

--- source/active_learning/system_under_learning/python/python_test.py
+++ source/active_learning/system_under_learning/python/python_test.py
@@ -1,8 +1,28 @@
 x = 0
 
-print("Outside")
+print("Printing from within the script")
 
-def test(i: int):
-    x += i
-    print("Inside")
+def test_no_arg():
+    print("Inside test no arg")
+    return 1
+
+def test_with_global():
+    global x
+
+    x += 3
+    print("Inside test no arg")
     return x
+
+def test_with_global_and_arg(i: int):
+    global x
+
+    x += i
+    print("Inside test with arg")
+    return x
+
+if __name__ == "__main__":
+    print(test_no_arg())
+    print(test_with_global())
+    print(test_with_global())
+    print(test_with_global_and_arg(4))
+    print(test_with_global_and_arg(5))
