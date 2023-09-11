9251938ebe49d7d9794564f8b32c73ab5ad1e6cf
Robert Baumgartner <r.baumgartner-1@tudelft.nl>
2023-09-11 16:23:36 +0200

    Finished the log-based alergia test

--- source/evaluation/log_alergia.h
+++ source/evaluation/log_alergia.h
@@ -17,29 +17,32 @@
 #include <unordered_map>
 
 class log_alergia_data: public evaluation_data {
-
+    friend class log_alergia;
 protected:
 
     REGISTER_DEC_DATATYPE(log_alergia_data);
     std::unordered_map<int, double> symbol_probability_map;
 
 public:
-    virtual void print_transition_label(iostream& output, int symbol);
-    virtual void print_state_label(iostream& output);
+    virtual void print_transition_label(iostream& output, int symbol) override;
+    //virtual void print_state_label(iostream& output) override;
 
-    virtual void update(evaluation_data* right);
-    virtual void undo(evaluation_data* right);
+    virtual void update(evaluation_data* right) override;
+    virtual void undo(evaluation_data* right) override;
 
-    virtual void initialize();
+    //virtual void initialize() override;
 
-    virtual void del_tail(tail *t);
-    virtual void add_tail(tail *t);
+    //virtual void del_tail(tail *t) override;
+    //virtual void add_tail(tail *t) override;
 
-    virtual void read_json(json& node);
-    virtual void write_json(json& node);
+    virtual void read_json(json& node) override;
+    virtual void write_json(json& node) override;
 
-    virtual int predict_symbol(tail*);
-    virtual double predict_symbol_score(int t);
+    virtual int predict_symbol(tail*) override;
+    virtual double predict_symbol_score(int t) override;
+
+    double get_probability(const int symbol);
+    void insert_probability(const int symbol, const double p);
 };
 
 class log_alergia: public evaluation_function {
@@ -47,13 +50,25 @@ class log_alergia: public evaluation_function {
 protected:
     REGISTER_DEC_TYPE(log_alergia);
 
+    double log_mu; 
+    double log_sqr_n;
+    double alpha;
+    double right;
+
+    double sum_diffs;
+
+    
 public:
 
-    virtual bool consistent(state_merger *merger, apta_node* left, apta_node* right);
+    virtual bool consistent(state_merger *merger, apta_node* left, apta_node* right) override;
 
-    virtual double compute_score(state_merger*, apta_node* left, apta_node* right);
-    virtual void reset(state_merger *merger);
+    virtual double compute_score(state_merger*, apta_node* left, apta_node* right) override;
+    virtual void reset(state_merger *merger) override;
+    virtual void initialize_before_adding_traces() override;
 
+    double get_log_mu(){
+        return log_mu;
+    }
 };
 
 #endif
