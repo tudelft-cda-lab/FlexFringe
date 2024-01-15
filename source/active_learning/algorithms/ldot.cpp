2acb2edac3a4ca77e192f0d2e5e8019d8e6610c0
Hielke Walinga <hielkewalinga@gmail.com>
2024-01-15 18:22:17 +0100

    Use sqldb_sul for ldot only.

--- source/active_learning/algorithms/ldot.cpp
+++ source/active_learning/algorithms/ldot.cpp
@@ -11,13 +11,18 @@
 
 #include "ldot.h"
 #include "loguru.hpp"
+#include "sqldb_sul.h"
 #include <fmt/format.h>
 
 void ldot_algorithm::run(inputdata& id) {
+    shared_ptr<sqldb_sul> my_sul = dynamic_pointer_cast<sqldb_sul>(sul);
+    if (my_sul == nullptr) {
+        throw logic_error("ldot only works with sqldb_sul.");
+    }
     LOG_S(INFO) << "Running the ldot algorithm.";
     vector<int> trace = {1};
-    int res = sul->query_trace_maybe(trace, id);
-    std::string log = fmt::format("Getting {0}: {1}", sul->get_sqldb().vec2str(trace), res);
+    int res = my_sul->query_trace_maybe(trace, id);
+    std::string log = fmt::format("Getting {0}: {1}", my_sul->get_sqldb().vec2str(trace), res);
     LOG_S(INFO) << log;
     // TODO: Implement the thing!
 }
