9ac93ba1561a6166d19a5bfbd02392def5562d16
Hielke Walinga <hielkewalinga@gmail.com>
2024-02-06 11:00:24 +0100

    Add trim utility file.

---
+++ source/misc/trim.h
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <cctype>
+#include <locale>
+
+// trim from start (in place)
+inline void ltrim(std::string& s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) { return !std::isspace(ch); }));
+}
+
+// trim from end (in place)
+inline void rtrim(std::string& s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !std::isspace(ch); }).base(), s.end());
+}
+
+// trim from both ends (in place)
+inline void trim(std::string& s) {
+    rtrim(s);
+    ltrim(s);
+}
+
+// trim from start (copying)
+inline std::string ltrim_copy(std::string s) {
+    ltrim(s);
+    return s;
+}
+
+// trim from end (copying)
+inline std::string rtrim_copy(std::string s) {
+    rtrim(s);
+    return s;
+}
+
+// trim from both ends (copying)
+inline std::string trim_copy(std::string s) {
+    trim(s);
+    return s;
+}
