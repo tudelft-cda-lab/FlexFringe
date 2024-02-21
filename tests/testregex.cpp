3e5a38e44d580c0f167c3d54d4d8edc596f75460
Hielke Walinga <hielkewalinga@gmail.com>
2024-02-21 19:58:32 +0100

    Finish regex test.

--- tests/testregex.cpp
+++ tests/testregex.cpp
@@ -1,10 +1,6 @@
-#undef _GLIBCXX_REGEX_STATE_LIMIT 
-#define _GLIBCXX_REGEX_STATE_LIMIT 100000000000000000
-
 #include "catch.hpp"
 #include "loguru.hpp"
 #include "parameters.h"
-#include <boost/regex.hpp>
 
 #include <iostream>
 #include <memory>
@@ -19,15 +15,14 @@
 #include "input/parsers/abbadingoparser.h"
 #include "state_merger.h"
 #include "main_helpers.h"
-#include "misc/printutil.h"
 
 
 
 TEST_CASE( "Test regex_builder functionality", "[regex]" ) {
     HEURISTIC_NAME = "evidence_driven";
     DATA_NAME = "edsm_data";
-    APTA_FILE = "data/tester1.final.json";
-    INPUT_FILE = "data/staminadata/1_training.txt";
+    APTA_FILE = "tests/data/regex/simple1.final.json";
+    INPUT_FILE = "tests/data/regex/simple.txt.dat";
     PREDICT_TYPE = true;
 
     inputdata id;
@@ -36,7 +31,6 @@ TEST_CASE( "Test regex_builder functionality", "[regex]" ) {
     evaluation_function *eval = get_evaluation();
     state_merger merger = state_merger(&id, eval, &the_apta);
 
-
     std::ifstream input_apta_stream{ APTA_FILE };
     LOG_S(INFO) << "Reading apta file - " << APTA_FILE;
     the_apta.read_json(input_apta_stream);
@@ -44,9 +38,9 @@ TEST_CASE( "Test regex_builder functionality", "[regex]" ) {
     auto coloring = std::make_tuple(PRINT_RED, PRINT_BLUE, PRINT_WHITE);
     regex_builder builder = regex_builder(the_apta, merger, coloring, sqldb::num2str);
     LOG_S(INFO) << "Finished building the regex builder";
-    std::map<int, boost::regex> regexes = {};
+    std::map<int, std::regex> regexes = {};
     for (const int type : inputdata_locator::get()->get_types()) {
-        regexes[type] = boost::regex{ "^(" + builder.to_regex(type) + ")$" };
+        regexes[type] = std::regex{ "^(" + builder.to_regex(type) + ")$" };
     }
 
     std::ifstream input_stream{ INPUT_FILE };
@@ -55,8 +49,8 @@ TEST_CASE( "Test regex_builder functionality", "[regex]" ) {
 
     for (auto* trace : id.trace_iterator(input_parser, parser_strategy)) {
         const std::string tr_str = sqldb::vec2str(trace->get_input_sequence(false, false));
-        bool match = boost::regex_match(tr_str, regexes[trace->type]);
-        cout << trace->type << " " << tr_str << " " << match << endl;
+        bool match = std::regex_match(tr_str, regexes[trace->type]);
+        CHECK( match );
     }
 }
 
