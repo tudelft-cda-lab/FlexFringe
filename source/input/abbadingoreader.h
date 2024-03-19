687ee0c1c623b8b24e187aeeaebfd36cee02d738
Tom Catshoek <tomcatshoek@zeelandnet.nl>
2024-03-19 14:28:59 +0100

    Add deprecation warnings to abbadingo inputdata

--- source/input/abbadingoreader.h
+++ source/input/abbadingoreader.h
@@ -4,13 +4,19 @@
 
 #include "input/inputdata.h"
 
+// Left in for compatibility, Do not use
+// TODO: remove usages and delete
 class abbadingo_inputdata: public inputdata {
 public:
+    [[deprecated]]
     void read(std::istream &input_stream);
-
+    [[deprecated]]
     void read_abbadingo_header(std::istream &input_stream);
+    [[deprecated]]
     void read_abbadingo_sequence(std::istream &input_stream, trace*);
+    [[deprecated]]
     void read_abbadingo_type(std::istream &input_stream, trace*);
+    [[deprecated]]
     void read_abbadingo_symbol(std::istream &input_stream, tail*);
 };
 
